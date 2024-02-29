43ef2c8ad02bab8385324e25827df5c57fd97459
H. Walid <h.walid@hexaee.com>
2024-02-29 17:58:38 +0100

    Finish singleton

---
+++ Memory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern
+{
+    public sealed class Memory //Restrict inheritance
+    {
+        private static Memory _instance;
+        public int Id { get; private set; }
+
+        private static readonly object _lock = new();
+        private Memory(){}
+
+        public static Memory Instance(int id)
+        {
+            if(_instance == null )
+            {
+                lock(_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new Memory();
+                        _instance.Id = id;  
+                    }
+                }
+            }//We use lock to create un single instance if we have multiple thread so if we have thread 1 and thread 2 thread 2 will wait until first thread is done 
+            return _instance;   
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Singleton_Design_Pattern;
+
+
+Thread t1 = new Thread(() =>
+{
+  Memory.Instance(1);
+});
+
+
+Thread t2 = new Thread(() =>
+{
+   Memory.Instance(2);
+});
+
+
+t1.Start(); 
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+Console.WriteLine(Memory.Instance(1).Id.ToString());
+Console.WriteLine(Memory.Instance(2).Id.ToString());
